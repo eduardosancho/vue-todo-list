e5e69c772ed69d878421f0b531e4d6241031835e
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-07-06 12:02:51 -0600

    Add Button reusable component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,31 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
+    <Button @click="onClick()" text="Add Task" color="green" />
   </header>
 </template>
 
 
 <script>
+import Button from './Button.vue';
+
 export default {
   name: "Header",
+    // props: ['title']
   props: {
     title: {
       type: String,
       default: "Hello World"
     } 
   },
-  // props: ['title']
+  components: {
+    Button
+  },
+  methods: {
+    onClick() {
+      console.log('click');
+    }
+  }
 };
 </script>
 
