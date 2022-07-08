b9e38633f9ab801c543015a7f244daaa495993b4
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-07-07 23:40:47 -0600

    Add persistency to toggleReminder

--- src/App.vue
+++ src/App.vue
@@ -37,8 +37,22 @@ export default {
 
       }
     },
-    toggleReminder(id) {
-      this.tasks = this.tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder } : task)
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id)
+
+      const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updatedTask)
+      })
+
+      const data = await res.json()
+
+      this.tasks = this.tasks.map(task => task.id === id ? { ...task, reminder: data.reminder } : task)
     },
     async addTask(task) {
       const res = await fetch('api/tasks', {
