26eb5fc9da11569fa4617adeeadfaae40601850a
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-07-07 23:40:47 -0600

    Add persistency to deleteTask

--- src/App.vue
+++ src/App.vue
@@ -27,9 +27,14 @@ export default {
     }
   },
   methods: {
-    deleteTask(id) {
+    async deleteTask(id) {
       if (confirm('Are you sure?')) {
-        this.tasks = this.tasks.filter(task => task.id !== id)
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE'
+        })
+
+        res.status === 200 ? (this.tasks = this.tasks.filter(task => task.id !== id)) : alert('Error when deleting task')
+
       }
     },
     toggleReminder(id) {
