ca74fc5b0a919dbc83395d40bcd93bf4ffdd0398
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-07-07 23:40:47 -0600

    Toggle Button color and text

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,8 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
-    <Button @toggle-add-task="$emit('toggle-add-task')" text="Add Task" color="green" />
+    <Button @toggle-add-task="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'" />
   </header>
 </template>
 
@@ -16,7 +17,8 @@ export default {
     title: {
       type: String,
       default: "Hello World"
-    }
+    },
+    showAddTask: Boolean
   },
   components: {
     Button
